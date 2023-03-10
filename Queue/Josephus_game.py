708b7b009f28fc25c8900cb6971a6bc816377af7
Soon Won Kwon <ksw6072@gmail.com>
2023-03-11 10:18:30 +1100

    Queue

--- Queue/Josephus_game.py
+++ Queue/Josephus_game.py
@@ -2,14 +2,11 @@ class Queue:
     def __init__(self):
         self.items=[]
         self.first_index=0
-        
     def enqueue(self,val):
         self.items.append(val)
-        
     def dequeue(self):
         if self.first_index==len(self.items):
             print('Queue is empty')
-            return None
         else:
             x=self.items[self.first_index]
             self.first_index+=1
@@ -18,4 +15,18 @@ class Queue:
         return self.items[self.first_index]
     def lenQ(self):
         return len(self.items)-self.first_index
+
+def Josephus(n,k): # n:the number of table k:th
+    Q=Queue()
     
+    for i in range(1,n+1):
+        Q.enqueue(i)
+        
+    
+    while (Q.lenQ!=1):
+        for j in range(1,k):
+            Q.enqueue(Q.dequeue)
+        Q.dequeue
+    return Q.dequeue
+
+        
