3ba2e6106f9c9013a81152e2e3e45e15352d4eef
Soon Won Kwon <ksw6072@gmail.com>
2023-03-14 23:36:32 +1100

    Data Structures

--- Stack/round_bracket.py
+++ Stack/round_bracket.py
@@ -1,47 +1,42 @@
 class Stack:
     def __init__(self):
         self.items=[]
-        
     def push(self,val):
         self.items.append(val)
         return self.items
-    
     def pop(self):
         try:
             return self.items.pop()
         except IndexError:
-            print('Stack is Empty')
-    
+            print('Stack is empty')
     def top(self):
         try:
             return self.items[-1]
         except IndexError:
             print('Stack is Empty')
-            
-    def __len__(self):
+    def len_stack(self):
         return len(self.items)
+    def is_empty(self):
+        return len(self.items)==0
     
-    def isEmpty(self):
-        return len(self)==0
-
-# round_bracket
-
-def parChecker(parSeq):
+def parChecker(preseq):
     S=Stack()
-    for symbol in parSeq:
-        if symbol == "(":
-            S.push(symbol)
-        else: # symbol==")"
-            if len(S)==0: # if nothing matched
+    for i in preseq:
+        if i=='(':
+            S.push(i)
+        else:
+            if S.is_empty():
                 return False
             else:
                 S.pop()
-    if len(S)==0:
+    if S.is_empty():
         return True
     else:
         return False
-
-list1=['(','(',')','(',')',')']
-print(parChecker(list1))
-       
     
+a=input('Enter:')
+print(parChecker(a))
+
+
+   
+            
