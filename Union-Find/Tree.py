2b4abf21fe1daa84a0ee95948b91cdc65d67af17
Soon Won Kwon <ksw6072@gmail.com>
2023-06-03 02:27:52 +1000

    Union-Find

--- Union-Find/Tree.py
+++ Union-Find/Tree.py
@@ -17,10 +17,11 @@ class disjointSet:
         
         # comparing size the sizes of the sets
         # the absolute value of the negative number represents the rank or height of the set
-        if self.data[x]<self.data[y]:
-            self.data[y]=x # y is the parent of x
+        # smaller rank is attached to the set with a larger rank
+        if self.data[x]<self.data[y]:            
+            self.data[y]=x 
         elif self.data[x]>self.data[y]:
-            self.data[x]=y # x is the parent of y
+            self.data[x]=y 
         else: # choosing one representative as the parent
             self.data[x]-=1 # decreasing its size by 1
             self.data[y]=x  
@@ -36,8 +37,10 @@ disjoint.union(4, 5)
 disjoint.union(5, 6)
 disjoint.union(6, 7)
 disjoint.union(8, 9)
-
+disjoint.union(0,8)
 print(disjoint.data)
 print(disjoint.size)
+
+        
 
         
